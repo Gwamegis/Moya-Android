1b8ae14922f3ffed884e314d531b6dc50ce6bc20
JIWON1923 <zest1923@gmail.com>
2023-04-14 17:18:40 +0900

    [Feat] Search View 설계

--- app/src/main/java/com/soi/moya/SearchFragment.kt
+++ app/src/main/java/com/soi/moya/SearchFragment.kt
@@ -5,55 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        val result = arrayOf("abc", "Anvi", "ajsi", "sjti", "wneit", "vnditl", "djiwtk")
+        val resultAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, result)
+        val listView = view.findViewById<ListView>(R.id.searchResultListView)
+        listView.adapter = resultAdapter
+
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                if (result.contains(query)) {
+                    resultAdapter.filter.filter(query)
                 }
+                return false
             }
+
+            override fun onQueryTextChange(nextText: String?): Boolean {
+                resultAdapter.filter.filter(nextText)
+                return false
+            }
+
+        })
+
+        return view
     }
 }
