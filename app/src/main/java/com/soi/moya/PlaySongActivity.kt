5667dc6231119a5a6d055c18825fe48eddf7694a
JIWON1923 <zest1923@gmail.com>
2023-11-18 22:58:50 +0900

    [Refactor] #31 - 함수로 분리

--- app/src/main/java/com/soi/moya/PlaySongActivity.kt
+++ app/src/main/java/com/soi/moya/PlaySongActivity.kt
@@ -24,6 +24,8 @@ class PlaySongActivity : AppCompatActivity() {
     private var isSongPrepared = false
     private var subColor: Int = 0
     private var pointColor: Int = 0
+    private var title = ""
+    private var id = ""
     private var url = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,88 +33,15 @@ class PlaySongActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         url = intent.getStringExtra("url")!!
+        id = intent.getStringExtra("id")!!
+        title = intent.getStringExtra("title")!!
 
         setContentView(R.layout.activity_play_song)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_play_song)
 
         settingUI()
-
-        mediaPlayer = MediaPlayer()
-        mediaPlayer?.setDataSource(url)
-        mediaPlayer?.prepareAsync()
-
-        mediaPlayer?.setOnPreparedListener {
-            isSongPrepared = true
-            mediaPlayer?.start()
-            startSeekBarUpdate()
-        }
-
-        mediaPlayer?.setOnErrorListener { mp, what, extra ->
-            // 오류 발생 시 처리하는 부분
-            Toast.makeText(this, "노래 재생 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
-            stopSeekBarUpdate()
-            mediaPlayer?.release()
-            mediaPlayer = null
-            true
-        }
-
-        mediaPlayer?.setOnCompletionListener {
-            mediaPlayer?.seekTo(0)
-            mediaPlayer?.start()
-        }
-
-        binding.playSongButton.setOnClickListener {
-            onTappedPlayButton()
-        }
-
-        binding.seekBar.setOnClickListener {
-            if (mediaPlayer !== null) mediaPlayer?.pause()
-        }
-
-        var color = ContextCompat.getColor(this, pointColor)
-        binding.seekBar.progressTintList = ColorStateList.valueOf(color)
-        binding.seekBar.thumbTintList = ColorStateList.valueOf(color)
-        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser && mediaPlayer != null && mediaPlayer?.isPlaying == true) mediaPlayer?.seekTo(progress)
-                binding.songCurrentTime.text = formatDuration(mediaPlayer!!.currentPosition)
-            }
-
-            override fun onStartTrackingTouch(seekBarseekBarseekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-
-        })
-
-        // scoll 위치에 따른 gradient
-        binding.scrollView.viewTreeObserver.addOnGlobalLayoutListener {
-            val scrollView = binding.scrollView
-            val scrollViewHeight = scrollView.height
-            val contentHeight = binding.songLyricLayout.height
-
-            if (binding.songLyric.height <= scrollViewHeight) {
-                // 스크롤뷰의 높이보다 TextView가 작을 때
-                binding.scrollTopOfGradientView.visibility = View.GONE
-                binding.scrollBottomOfGradientView.visibility = View.GONE
-            } else {
-                // 스크롤뷰의 높이보다 TextView가 클 때
-                scrollView.viewTreeObserver.addOnScrollChangedListener {
-                    val scrollY = scrollView.scrollY
-                    if (scrollY == 0) {
-                        binding.scrollTopOfGradientView.visibility = View.GONE
-                        binding.scrollBottomOfGradientView.visibility = View.VISIBLE
-                    } else if (scrollY + scrollViewHeight == contentHeight) {
-                        binding.scrollTopOfGradientView.visibility = View.VISIBLE
-                        binding.scrollBottomOfGradientView.visibility = View.GONE
-                    } else {
-                        binding.scrollTopOfGradientView.visibility = View.VISIBLE
-                        binding.scrollBottomOfGradientView.visibility = View.VISIBLE
-                    }
-                }
-            }
-        }
+        initializeMediaPlayer()
+        setupEventListeners()
     }
 
     private fun onTappedPlayButton() {
@@ -171,8 +100,52 @@ class PlaySongActivity : AppCompatActivity() {
         binding.songLyric.text = intent.getStringExtra("lyrics")?.replace("\\n", "\n")
         binding.scrollTopOfGradientView.background = topOfGradientDrawable
         binding.scrollBottomOfGradientView.background = bottomOfGradientDrawable
-
         window.statusBarColor = ContextCompat.getColor(this, subColor)
+
+        val color = ContextCompat.getColor(this, pointColor)
+        binding.seekBar.progressTintList = ColorStateList.valueOf(color)
+        binding.seekBar.thumbTintList = ColorStateList.valueOf(color)
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser && mediaPlayer != null && mediaPlayer?.isPlaying == true) mediaPlayer?.seekTo(progress)
+                binding.songCurrentTime.text = formatDuration(mediaPlayer!!.currentPosition)
+            }
+
+            override fun onStartTrackingTouch(seekBarseekBarseekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+        // scoll 위치에 따른 gradient
+        binding.scrollView.viewTreeObserver.addOnGlobalLayoutListener {
+            val scrollView = binding.scrollView
+            val scrollViewHeight = scrollView.height
+            val contentHeight = binding.songLyricLayout.height
+
+            if (binding.songLyric.height <= scrollViewHeight) {
+                // 스크롤뷰의 높이보다 TextView가 작을 때
+                binding.scrollTopOfGradientView.visibility = View.GONE
+                binding.scrollBottomOfGradientView.visibility = View.GONE
+            } else {
+                // 스크롤뷰의 높이보다 TextView가 클 때
+                scrollView.viewTreeObserver.addOnScrollChangedListener {
+                    val scrollY = scrollView.scrollY
+                    if (scrollY == 0) {
+                        binding.scrollTopOfGradientView.visibility = View.GONE
+                        binding.scrollBottomOfGradientView.visibility = View.VISIBLE
+                    } else if (scrollY + scrollViewHeight == contentHeight) {
+                        binding.scrollTopOfGradientView.visibility = View.VISIBLE
+                        binding.scrollBottomOfGradientView.visibility = View.GONE
+                    } else {
+                        binding.scrollTopOfGradientView.visibility = View.VISIBLE
+                        binding.scrollBottomOfGradientView.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
     }
 
     private fun startSeekBarUpdate() {
@@ -203,5 +176,57 @@ class PlaySongActivity : AppCompatActivity() {
         val seconds = TimeUnit.MILLISECONDS.toSeconds(duration.toLong()) % 60
         return String.format("%02d:%02d", minutes, seconds)
     }
-}
 
+    // Media player 관련
+
+
+    private fun initializeMediaPlayer() {
+        mediaPlayer = createMediaPlayer()
+        mediaPlayer?.prepareAsync()
+
+        mediaPlayer?.setOnPreparedListener {
+            onMediaPlayerPrepared()
+        }
+
+        mediaPlayer?.setOnErrorListener { _, _, _ ->
+            handleMediaPlayerError()
+            true
+        }
+
+        mediaPlayer?.setOnCompletionListener {
+            restartMediaPlayer()
+        }
+    }
+
+    private fun createMediaPlayer(): MediaPlayer {
+        val player = MediaPlayer()
+        player.setDataSource(url)
+        return player
+    }
+
+    private fun onMediaPlayerPrepared() {
+        isSongPrepared = true
+        mediaPlayer?.start()
+        startSeekBarUpdate()
+    }
+
+    private fun handleMediaPlayerError() {
+        Toast.makeText(this, "노래 재생 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+        stopSeekBarUpdate()
+        mediaPlayer?.release()
+    }
+
+    private fun restartMediaPlayer() {
+        mediaPlayer?.seekTo(0)
+        mediaPlayer?.start()
+    }
+
+    private fun setupEventListeners() {
+        binding.playSongButton.setOnClickListener { onTappedPlayButton() }
+
+        binding.seekBar.setOnClickListener {
+            if (mediaPlayer?.isPlaying == true) mediaPlayer?.pause()
+        }
+    }
+
+}
