5937852847fbfc9ddf5a9980c52b12403ef1b4c6
JIWON1923 <zest1923@gmail.com>
2024-01-04 03:00:22 +0900

    [#33] [Feat] - Bottom Navigation 구현

--- app/src/main/java/com/soi/moya/ui/MainActivity.kt
+++ app/src/main/java/com/soi/moya/ui/MainActivity.kt
@@ -3,14 +3,13 @@ package com.soi.moya.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.soi.moya.base.BaseComposeActivity
 import com.soi.moya.ui.main.MainScreen
 
-class MainActivity : ComponentActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MainScreen()
-        }
+class MainActivity : BaseComposeActivity() {
+    @Composable
+    override fun Content() {
+        MainScreen()
     }
 }
