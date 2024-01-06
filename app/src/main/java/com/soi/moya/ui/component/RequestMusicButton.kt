1a57209047ba450c87bea26f3dfaa62f83cc46bf
JIWON1923 <zest1923@gmail.com>
2024-01-07 07:34:03 +0900

    [#37] 응원가 신청 버튼 component

---
+++ app/src/main/java/com/soi/moya/ui/component/RequestMusicButton.kt
@@ -0,0 +1,52 @@
+package com.soi.moya.ui.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.soi.moya.R
+
+@Composable
+fun RequestMusicButton(color: Color) {
+    Box(
+        modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+
+        OutlinedButton(
+            onClick = {
+                      // TODO: webview 연결
+            },
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = color
+            ),
+            border = BorderStroke(1.dp, color),
+            shape = RoundedCornerShape(30.dp),
+            contentPadding = PaddingValues(horizontal = 40.dp, vertical = 8.dp)
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.send_icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 8.dp)
+                    .size(20.dp)
+            )
+            Text(text = stringResource(id = R.string.request_music))
+        }
+    }
+}
