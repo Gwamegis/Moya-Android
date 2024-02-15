060a14332459c090cf8a5031784923c118cafb91
jeonjimin <cjjunn0827@gmail.com>
2024-02-15 20:32:20 +0900

    [Feat] 현재 선택한 팀 활성화
    - 팀 선택 화면에서 현재 선택한 팀이 있을 경우 내팀 표시하도록 userPreference값과 연결

--- app/src/main/java/com/soi/moya/ui/select_team/SelectTeamViewModel.kt
+++ app/src/main/java/com/soi/moya/ui/select_team/SelectTeamViewModel.kt
@@ -1,10 +1,8 @@
 package com.soi.moya.ui.select_team
 
 import android.app.Application
-import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.soi.moya.models.Team
 import com.soi.moya.models.UserPreferences
@@ -13,7 +11,21 @@ import kotlinx.coroutines.launch
 class SelectTeamViewModel(application: Application): AndroidViewModel(application = Application()) {
     val teams = Team.values()
     var selectedTeam = mutableStateOf<Team?>(null)
-    val userPreferences = UserPreferences(application)
+
+    private val _userPreferences = UserPreferences(application)
+
+    init {
+        observeUserPreference()
+    }
+
+    private fun observeUserPreference() {
+        viewModelScope.launch {
+            _userPreferences.getSelectedTeam.collect { team ->
+                selectedTeam.value = team?.let { Team.valueOf(it) }
+            }
+        }
+    }
+
     fun onTeamSelected(currentTeam: Team) {
         if(selectedTeam.value == currentTeam) {
             selectedTeam.value = null
@@ -23,8 +35,7 @@ class SelectTeamViewModel(application: Application): AndroidViewModel(applicatio
     }
     fun onClickNext() {
         viewModelScope.launch {
-            selectedTeam.value?.let { userPreferences.saveSelectedTeam(team = it) }
+            selectedTeam.value?.let { _userPreferences.saveSelectedTeam(team = it) }
         }
-        println("선택한 팀: ${selectedTeam.value}")
     }
 }
