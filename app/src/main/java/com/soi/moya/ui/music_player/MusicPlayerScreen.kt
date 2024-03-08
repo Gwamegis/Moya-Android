d4771b81b0a017eec64f695b87436e3fb730e118
jeonjimin <cjjunn0827@gmail.com>
2024-03-08 21:46:39 +0900

    [Feat] - 음악 재생화면 하단 여백 조정

--- app/src/main/java/com/soi/moya/ui/music_player/MusicPlayerScreen.kt
+++ app/src/main/java/com/soi/moya/ui/music_player/MusicPlayerScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -316,7 +317,8 @@ fun MusicPlayerBottomButtonView(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .navigationBarsPadding(),
         contentAlignment = Alignment.Center,
     ) {
         Image(
